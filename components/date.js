5ea179de0a4347487d5c5e9c571420a1ec692024
Paul Kischlat <paul.kischlat@postcode-lotterie.de>
2021-10-21 15:22:27 +0200

    German Date Formatting

--- components/date.js
+++ components/date.js
@@ -1,6 +1,11 @@
 import { parseISO, format } from "date-fns";
+import { de } from "date-fns/locale";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "d. LLLL yyyy")}</time>;
+  return (
+    <time dateTime={dateString}>
+      {format(date, "d. LLLL yyyy", { locale: de })}
+    </time>
+  );
 }
