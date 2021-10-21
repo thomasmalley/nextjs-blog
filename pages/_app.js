d8cee1baa0d82951b852b6e03b1727bf665f1297
Paul Kischlat <paul.kischlat@postcode-lotterie.de>
2021-10-21 15:00:15 +0200

    Disable Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import "../styles/global.css";
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
+import "/styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
