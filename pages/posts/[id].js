d3865dc1443bff68ff814a32eb4824f30639be60
Paul Kischlat <paul.kischlat@postcode-lotterie.de>
2021-10-21 16:06:17 +0200

    CSS Polishing

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,20 +2,21 @@ import Layout from "/components/layout";
 import Date from "/components/date";
 import Head from "next/head";
 
+import utilStyles from "/styles/utils.module.css";
+
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
